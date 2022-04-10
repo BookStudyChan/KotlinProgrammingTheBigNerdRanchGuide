90ebd6133f0adde9f8070e6dfd222a9fcada1abd
changyuPark <blue_rose17@naver.com>
2022-04-10 18:09:46 +0900

    코틀린 실습 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,39 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val name ="마드리갈"
+    val healthPoint = 89
+    var isBlessed = true
+    val isImmortal = false
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val auraColor = auraColor(isBlessed, healthPoint, isImmortal)
+    val healthStatus = formatHealthStatus(healthPoint, isBlessed)
+
+    //플레이어의 상태 출력
+    printPlayerStatus(auraColor,isBlessed,name,healthStatus)
+
+}
+
+private fun formatHealthStatus(healthPoint:Int,isBlessed:Boolean) :String{
+    val healthStatus = when (healthPoint){
+        100 -> "최상의 상태"
+        in 90..99 -> "약간의 찰과상이 있음"
+        in 75..89 -> if (isBlessed){
+            "경미한 상처만 있음."
+        }else{
+            "경미한 상처만있음."
+        }
+        in 15..74->"많이 다친 것 같음"
+        else -> "최악의 상태임!"
+    }
+    return healthStatus
+}
+
+private fun printPlayerStatus(auraColor:String, isBlessed: Boolean, name:String,healthStatus:String){
+    println("(Aura:$auraColor)" + "(Blessed : ${if (isBlessed)"YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed:Boolean, healthPoint: Int,isImmortal:Boolean):String{
+    val auraVisible = isBlessed && healthPoint > 50 || isImmortal
+    val auraColor = if(auraVisible) "GREEN" else "NONE"
+    return auraColor
 }
